7fccd81922548241f3512e53fc9aa8ab66942d4f
Sygram L <sygramdev@gmail.com>
2025-11-26 07:04:53 +0000

    File changes

--- src/components/user/UserSubscriptions.jsx
+++ src/components/user/UserSubscriptions.jsx
@@ -523,11 +523,11 @@ function SubscriptionCard({
               </Button>
             )}
             <Button
-              variant="outline"
               size="sm"
-              className="text-red-600 hover:text-red-700 hover:bg-red-50 border-red-200"
+              className="bg-red-600 hover:bg-red-700 text-white"
               onClick={onCancelSubscription}
             >
+              <XCircle className="w-4 h-4 mr-2" />
               {t.cancelSubscription}
             </Button>
           </div>
