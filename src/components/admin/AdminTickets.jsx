fffc06ce24380cb47260ac6c15857e77734a2bd4
Sygram L <sygramdev@gmail.com>
2025-12-01 12:11:54 +0000

    File changes

--- src/components/admin/AdminTickets.jsx
+++ src/components/admin/AdminTickets.jsx
@@ -214,7 +214,7 @@ export default function AdminTickets() {
 
       {/* Chat Dialog */}
       <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-        <DialogContent className="max-w-2xl p-0 overflow-hidden max-h-[85vh]">
+                    <DialogContent className="max-w-2xl w-[95vw] sm:w-full p-0 overflow-hidden max-h-[85vh]">
           {selectedTicket && currentUser && (
             <TicketChat 
               ticket={selectedTicket}
