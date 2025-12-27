93efce315d86017c66a41489b3b04d7f6d28c061
Sygram L <sygramdev@gmail.com>
2025-12-27 20:38:03 +0000

    Manual code change

--- src/components/pricing/PlanComparisonTable.jsx
+++ src/components/pricing/PlanComparisonTable.jsx
@@ -180,7 +180,7 @@ export default function PlanComparisonTable() {
   };
 
   return (
-    <div className="pt-8 pb-16 bg-gradient-to-b from-white to-gray-50">
+    <div className="pb-16 bg-gradient-to-b from-white to-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="text-center mb-8">
