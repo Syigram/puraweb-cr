33f71be7771ae534db38d65590c97aa741408acc
Sygram L <sygramdev@gmail.com>
2025-12-29 23:52:52 +0000

    Imagen fresca y moderna

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,13 +1,13 @@
 import AdminDashboard from './pages/AdminDashboard';
 import Checkout from './pages/Checkout';
 import Home from './pages/Home';
+import Nosotros from './pages/Nosotros';
 import NosotrosManifiesto from './pages/NosotrosManifiesto';
 import PaymentSuccess from './pages/PaymentSuccess';
 import Planes from './pages/Planes';
 import Servicios from './pages/Servicios';
 import Support from './pages/Support';
 import UserDashboard from './pages/UserDashboard';
-import Nosotros from './pages/Nosotros';
 import __Layout from './Layout.jsx';
 
 
@@ -15,13 +15,13 @@ export const PAGES = {
     "AdminDashboard": AdminDashboard,
     "Checkout": Checkout,
     "Home": Home,
+    "Nosotros": Nosotros,
     "NosotrosManifiesto": NosotrosManifiesto,
     "PaymentSuccess": PaymentSuccess,
     "Planes": Planes,
     "Servicios": Servicios,
     "Support": Support,
     "UserDashboard": UserDashboard,
-    "Nosotros": Nosotros,
 }
 
 export const pagesConfig = {
