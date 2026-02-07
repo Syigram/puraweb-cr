6249197b99a1794c59640165a95d5449b54d31cb
Sygram L <sygramdev@gmail.com>
2026-02-07 07:32:25 +0000

    Manual code change

--- functions/whatsappContactNotification.ts
+++ functions/whatsappContactNotification.ts
@@ -39,7 +39,7 @@ Deno.serve(async (req) => {
           type: 'template',
           template: {
             name: 'contacto_notificacion_puraweb',
-            language: { code: 'es_CRI' },
+            language: { code: 'es_CR' },
             components: [
               {
                 type: 'body',
