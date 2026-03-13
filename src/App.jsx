0899ac1375e6efc33e8f54187c8317996829aeb9
Sygram L <sygramdev@gmail.com>
2026-03-13 03:38:01 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,7 @@ const AuthenticatedApp = () => {
         {Object.entries(Pages).map(([path, Page]) => (
           <Route key={path} path={`/${path}`} element={<Page />} />
         ))}
+        <Route path="/ComoTrabajamos" element={<LayoutWrapper currentPageName="ComoTrabajamos"><ComoTrabajamos /></LayoutWrapper>} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </LayoutWrapper>
