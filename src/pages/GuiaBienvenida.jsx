3f96b72e5bde65ca25a7e8edc6de49e4ac8c3174
Sygram L <sygramdev@gmail.com>
2026-03-14 06:57:00 +0000

    File changes

--- src/pages/GuiaBienvenida.jsx
+++ src/pages/GuiaBienvenida.jsx
@@ -24,6 +24,7 @@ import {
 } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 import Contact from "@/components/home/Contact";
+import TransparencyIconsShowcase from "@/components/workflow/TransparencyIcons";
 
 // Memoized animation config - defined outside component to prevent recreation
 const fadeInUp = {
