6ba4854ffafb19452285476f8c6bb2316f2b35ea
Sygram L <sygramdev@gmail.com>
2026-03-14 07:14:14 +0000

    File changes

--- src/pages/GuiaBienvenida.jsx
+++ src/pages/GuiaBienvenida.jsx
@@ -34,6 +34,7 @@ const fadeInUp = {
 };
 
 function ComoTrabajamos() {
+  const [selectedIconId, setSelectedIconId] = React.useState('network');
   const { language } = useLanguage();
 
   // Memoize content object - only recreated when dependencies change
